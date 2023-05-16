649727a564aa1c77de648868f4c341b8fc9f8033
Urvik07 <73632473+Urvik07@users.noreply.github.com>
2023-05-16 20:50:57 +0530

    Add files via upload

---
+++ build/precache-manifest.8b2d3e15074913d1c0d55bcc243ed968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101c3daf73db63e7e396725590c312d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e4c9fb563f6f43a62f5",
+    "url": "/static/css/main.fb7fe8de.chunk.css"
+  },
+  {
+    "revision": "985ca91750ccc9f31871",
+    "url": "/static/js/2.699c3045.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/static/js/2.699c3045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4c9fb563f6f43a62f5",
+    "url": "/static/js/main.1a965506.chunk.js"
+  },
+  {
+    "revision": "6ca49defa095afd3e9c1",
+    "url": "/static/js/runtime-main.f9911b94.js"
+  }
+]);
